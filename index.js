4f44a6e1bbf3023824f9dadff25654591f4efc13
theziko1 <zakariaghachim96@gmail.com>
2024-03-25 21:08:07 +0000

    initialisation du projet

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express()
 app.use(express.json())
 
 
+app.use("/",(req,res)=>{
+    res.send("first APi")
+})
+
+
 
 app.listen(3000,()=>{
     console.log("🚀 ~ server listen ~ in PORT 3000")   
