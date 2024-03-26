f36c38668f2b77499d7f99121ae7076f7cc69bbd
FarhanMohammadDev <farhanmohammad0677@gmail.com>
2024-03-26 21:34:15 +0100

    add validation joi

--- models/User.js
+++ models/User.js
@@ -1,24 +1,72 @@
-import { Schema , model } from "mongoose"
+import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema({
-    username : {
-        type : String,
-        required : true,
-        unique : true,
-    },
-    email : {
-        type : String,
-        unique : true,
-    },
-    password : {
-        type : String,
-        required : true
-    },
-    age : {
-        type : Number
-    }
-    
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+});
 
-})
 
-export const User  = model('User',UserSchema)
+/**
+ * @desc function validate Register User
+ * @access public
+ *
+ */
+function validateRegisterUser(obj) {
+  const schema = Joi.object({
+    email: Joi.string().trim().email().min(5).max(100).required(),
+    username: Joi.string().trim().alphanum().min(3).max(200).required(),
+    password: Joi.string().trim().alphanum().min(3).max(200).required(),
+    age: Joi.number().trim().min(3).max(200).required(),
+  });
+
+  return schema.validate(obj);
+}
+
+/**
+ * @desc function validate Update User
+ * @access public
+ */
+function validateUpdateUser(obj) {
+  const schema = Joi.object({
+    email: Joi.string().trim().email().min(5).max(100),
+    username: Joi.string().trim().alphanum().min(3).max(200),
+    password: Joi.string().trim().alphanum().min(3).max(200),
+    age: Joi.number().trim().min(3).max(200),
+  });
+
+  return schema.validate(obj);
+}
+
+/**
+ * @desc function validate Login user
+ * @access public
+ */
+function validateLoginUser(obj) {
+  const schema = Joi.object({
+    email: Joi.string().trim().email().min(5).max(100).required(),
+    password: Joi.string().trim().alphanum().min(3).max(200).required(),
+  });
+
+  return schema.validate(obj);
+}
+const User = mongoose.model("User", UserSchema);
+module.exports = {
+  User,
+  validateRegisterUser,
+  validateUpdateUser,
+  validateLoginUser,
+};
