94eb5e240a4f5f20b3a8f38ce71674a68d90bb89
mibanas <sanabi_mohamed@hotmail.fr>
2024-03-27 10:29:01 +0000

    add test

---
+++ test-server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { config } from "dotenv"
+import { connect } from "mongoose";
+import userRouter from "./routes/User.js"
+
+config()
+
+const PORT = process.env.PORT
+
+const app = express()
+
+app.use(express.json())
+app.use("/",userRouter)
+
+connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("connected to the database")
+}) 
+.catch ((error)=> {
+    console.log("connexion failed",error) 
+})
+
+export default app
